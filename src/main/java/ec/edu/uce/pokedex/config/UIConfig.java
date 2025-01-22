9493d271255e0567520a164464656de69e136f66
Erenriquezp <erenriquezp@uce.edu.ec>
2025-01-21 19:51:38 -0500

    added UI configs

---
+++ src/main/java/ec/edu/uce/pokedex/config/UIConfig.java
@@ -0,0 +1,25 @@
+package ec.edu.uce.pokedex.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import java.awt.*;
+
+@Configuration
+public class UIConfig {
+
+    public Font titleFont() {
+        return new Font("Arial", Font.BOLD, 24);
+    }
+
+    public Font labelFont() {
+        return new Font("Arial", Font.PLAIN, 16);
+    }
+
+    public Color primaryColor() {
+        return Color.BLUE;
+    }
+
+    public Color secondaryColor() {
+        return Color.WHITE;
+    }
+}
---
+++ src/main/java/ec/edu/uce/pokedex/utils/ComponentFactory.java
@@ -0,0 +1,28 @@
+package ec.edu.uce.pokedex.utils;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ComponentFactory {
+
+    public static JButton createButton(String text, int fontSize, Color background, Color foreground) {
+        JButton button = new JButton(text);
+        button.setFont(new Font("Arial", Font.BOLD, fontSize));
+        button.setBackground(background);
+        button.setForeground(foreground);
+        return button;
+    }
+
+    public static JLabel createLabel(String text, int fontSize, int alignment) {
+        JLabel label = new JLabel(text, alignment);
+        label.setFont(new Font("Arial", Font.BOLD, fontSize));
+        return label;
+    }
+
+    public static JPanel createSearchPanel(JTextField searchField, JButton searchButton) {
+        JPanel searchPanel = new JPanel(new BorderLayout());
+        searchPanel.add(searchField, BorderLayout.CENTER);
+        searchPanel.add(searchButton, BorderLayout.EAST);
+        return searchPanel;
+    }
+}
